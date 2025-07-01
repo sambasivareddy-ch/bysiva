078891ae8cfe853f4029aaf0ffd7c9701f7e835d
V N G Samba Siva Reddy Chinta <151616423+sambasivareddy-ch@users.noreply.github.com>
2025-07-01 00:56:24 +0000

    Added Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Blog from './pages/Blog';
 import BlogPost from './pages/BlogPost';
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path='/' element={<Blog />} exact />
       <Route path="/blog/:slug" element={<BlogPost />} exact />
       <Route path='*' element={<Blog />} />
+      <Analytics />
     </Routes>
   );
 }
